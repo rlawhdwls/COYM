f67b7787e625c7df64417da82dbca14a87bfeff7
rlawhdwls <50696204+rlawhdwls@users.noreply.github.com>
2021-03-05 23:45:49 +0900

    210305
    210305

---
+++ main.js
@@ -0,0 +1,10 @@
+const toggle = document.querySelector('.navbar__toggle');
+const menu = document.querySelector('.navbar__menu');
+const icons = document.querySelector('.navbar__icons');
+
+toggle.addEventListener('click',()=>{
+    menu.classList.toggle('active');
+    icons.classList.toggle('active');
+
+
+});
